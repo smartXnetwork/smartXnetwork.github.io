1cbd6e81bf1e17a521c895df3e3cc62a8d3bf7f2
Abhimanyu Godara <abhimanyu@Abhimanyus-MacBook-Air.local>
2019-05-12 14:26:44 +0530

    test

--- index.js
+++ index.js
@@ -1422,6 +1422,7 @@ const smartX = ( IPFS , ORBITDB ) => {
         }
 
         async function loadToken () {
+            console.log(document.location.href)
             if (document.location.href !== 'https://smartxnetwork.github.io') {
                 let url = document.location.search;
                 let tokenID = url.replace("?", '');
